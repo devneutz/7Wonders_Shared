efe7f02df7acba47d91830ee744093641eec4696
Joel N <joel.neutzner@hotmail.com>
2019-03-30 16:02:23 +0100

    Interface IBoard beschrieben, Enum ResourceType

---
+++ src/ch/fhnw/sevenwonders/enums/ResourceType.java
@@ -0,0 +1,5 @@
+package ch.fhnw.sevenwonders.enums;
+
+public enum ResourceType {
+	Wood,Stone,Clay,Ore,Glas,Papyrus,Cloth,Coin,MilitaryMight,VictoryPoint,StonePanel,Compasses
+}
--- src/ch/fhnw/sevenwonders/interfaces/IBoard.java
+++ src/ch/fhnw/sevenwonders/interfaces/IBoard.java
@@ -1,6 +1,17 @@
 package ch.fhnw.sevenwonders.interfaces;
 
+import java.util.ArrayList;
+
+import ch.fhnw.sevenwonders.enums.*;
+
 public interface IBoard {
 	
-
+	// Methode zum Überprüfen, ob mit den angegebenen Ressourcen der Pyramidenteil gebaut werden kann
+	Boolean canBuild(int inStep, ArrayList<ResourceType> inResources);
+	
+	String getImageName();
+	
+	ArrayList<ResourceType> getStepOneValue();
+	
+	ArrayList<ResourceType> getStepTwoValue();
 }
