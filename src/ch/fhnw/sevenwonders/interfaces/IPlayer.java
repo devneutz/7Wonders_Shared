741e2dd7edeee6f29e1fa5d1daa5a5b36785fef6
gabrieldecastilho <44000769+gabrieldecastilho@users.noreply.github.com>
2019-04-01 21:43:44 +0200

    Player Interface ergänzt
    - Erstellung aller Methoden analog Klassendiagramm

--- src/ch/fhnw/sevenwonders/interfaces/IPlayer.java
+++ src/ch/fhnw/sevenwonders/interfaces/IPlayer.java
@@ -1,5 +1,46 @@
 package ch.fhnw.sevenwonders.interfaces;
+import ch.fhnw.sevenwonders.enums.*;
+import java.util.ArrayList;
 
-public interface IPlayer {
-
+public interface IPlayer {	
+	
+	// Methode zur Festlegung des Password Hash
+	void setPasswordHash(String pwHash);
+	
+	// Methode zur Festlegung des Spielernamen
+	void setName(String name);
+	
+	// Methode zum Auslesen des Spielernamen
+	String getName();
+	
+	// Methode zum Auslesen der Ressourcen welche der Spieler zur Verfügung hat
+	ArrayList<ResourceType> getResource(ResourceType resource);
+	
+	// Methode zum Auslesen welches Board der Spieler nutzt
+	IBoard getBoard();
+	
+	// Methode zur Überprüfung ob das definierte Passwort gültig ist
+	Boolean isPasswordValid();
+	
+	// ??Joel fragen was damit gemeint ist ?? Wieso als Rückgabe boolean und nicht das Objekt Card ?
+	Boolean getCardPlayed();
+	
+	// ??Joel fragen was damit gemeint ist ?? Wieso als Eingabe boolean und nicht das Objekt Card ?
+	void setCardPlayed(Boolean cardPlayed);
+	
+	// Methode zum Hinzufügen des Spielers in die mitgegebene Lobby
+	void joinLobby(String lobby); //-> ILobby muss noch erstellt werden, danach umbenenen !
+	
+	// Methode zur Erstellung der Lobby
+	void creatLobby(String lobbyName);
+	
+	// Methode zur Löschung der mitgegebenen Lobby
+	void deleteLobby (String lobby); //-> ILobby muss noch erstellt werden, danach umbenenen !
+	
+	// Methode zum Aussteigen aus einer Lobby
+	void leaveLobby(String lobbyName);
+	
+	// Methode zum Vergleichen der Spieler in einem Spiel um zu identifizieren welcher Spieler gewonnen hat
+	int compareTo(IPlayer player);	
+	
 }
