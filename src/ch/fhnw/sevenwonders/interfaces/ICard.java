e5215c13c8207fb75105c4a946fb435715f89a8f
theMatto94 <48686440+theMatto94@users.noreply.github.com>
2019-04-01 23:41:32 +0200

    Update ICard.java

--- src/ch/fhnw/sevenwonders/interfaces/ICard.java
+++ src/ch/fhnw/sevenwonders/interfaces/ICard.java
@@ -1,5 +1,36 @@
 package ch.fhnw.sevenwonders.interfaces;
 
+import java.util.ArrayList;
+
+import ch.fhnw.sevenwonders.enums.Age;
+import ch.fhnw.sevenwonders.enums.CardType;
+import ch.fhnw.sevenwonders.enums.ResourceType;
+import ch.fhnw.sevenwonders.enums.ValueCalculationType;
+
 public interface ICard {
+	
+	// Gibt das Zeitalter aus dem Enum "Age" zurück.
+	Age getAge();
+	
+	// Gibt den Kartentyp aus dem Enum "CardType" zurück.
+	CardType getCardType();
+	
+	// Wir die Karte zum Bau des Weltwunders verwendet?
+	Boolean getUsedForPyramid();
+	
+	// Gibt die Kosten einer bestimmten Karte zurück, welche in der Liste "ResourceType" gespeichert ist.
+	ArrayList<ResourceType> getCost();
+	
+	// Gibt den Wert einer bestimmten Karte zurück, welche in der Liste "ResourceType" gespeichert ist.
+	ArrayList<ResourceType> getValue();
+	
+	// Kann die gewünschte Karte gespielt werden bzw. sind genügend Resourcen vorhanden? Dieser Wert ist ebenfalls in der Liste "ResourceType" gespeichert.
+	Boolean isPlayable(ArrayList<ResourceType> array);
+	
+	// Gibt einen Wert aus dem enum "ValueCalculationType" zurück. Dieser Wert entscheidet wie die Resource zu zählen ist.
+	ValueCalculationType getValueCalculationType();
+	
+	// Gibt den Namen der entsprechenden Resource zurück
+	String getImageName();
 
 }
