7635bb2c5575632e77cb34c49ffa3b99e84b62e2
ismaelliu <48707235+ismaelliu@users.noreply.github.com>
2019-04-02 16:45:09 +0200

    Create ILobby.java

---
+++ src/ch/fhnw/sevenwonders/interfaces/ILobby.java
@@ -0,0 +1,10 @@
+package ch.fhnw.sevenwonders.interfaces;
+
+import java.util.List;
+
+public interface ILobby {
+		
+		// startet die Lobby mit den angehörigen Playern als Liste.  
+		void startLobby(List<IPlayer> list);
+
+}
