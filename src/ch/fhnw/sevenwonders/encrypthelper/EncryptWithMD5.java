32311a6bed018dadcad9e55e781bb8248e2ac38f
gabrieldecastilho <44000769+gabrieldecastilho@users.noreply.github.com>
2019-04-28 18:05:51 +0200

    Kommentare hinzugefügt
    Kommentare hinzugefügt

--- src/ch/fhnw/sevenwonders/encrypthelper/EncryptWithMD5.java
+++ src/ch/fhnw/sevenwonders/encrypthelper/EncryptWithMD5.java
@@ -7,31 +7,19 @@ import java.util.logging.Logger;
 
 /**
  * 
- * @author https://stackoverflow.com/questions/10696432/encryption-of-password-in-java-or-mysql
+ * @author Gabriel de Castilho -> Source from
+ *         https://stackoverflow.com/questions/10696432/encryption-of-password-in-java-or-mysql
+ * 
+ *         Diese Klasse stellt die Hashfunktion MD5 zur Verf�gung, welche f�r
+ *         die Passwortverschl�sselung verwendet wird.
  *
  */
 
 public class EncryptWithMD5 {
-	
+
 	private static MessageDigest md;
 
-	   public static String cryptWithMD5(String pass){
-	    try {
-	        md = MessageDigest.getInstance("MD5");
-	        byte[] passBytes = pass.getBytes();
-	        md.reset();
-	        byte[] digested = md.digest(passBytes);
-	        StringBuffer sb = new StringBuffer();
-	        for(int i=0;i<digested.length;i++){
-	            sb.append(Integer.toHexString(0xff & digested[i]));
-	        }
-	        return sb.toString();
-	    } catch (NoSuchAlgorithmException ex) {
-	        Logger.getLogger(EncryptWithMD5.class.getName()).log(Level.SEVERE, null, ex);
-	    }
-	        return null;
 
-
-	   }
+	}
 
 }
